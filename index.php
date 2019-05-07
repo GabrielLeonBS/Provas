4a9e4151269ce35767d746e8c47eb44eab04215d
GabrielLeonBS <48722949+GabrielLeonBS@users.noreply.github.com>
2019-05-07 11:52:56 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+ include("cabecalho.php");
+ ?>
+
+<div class="container" style="margin-top:30px;">
+  <div class="row">
+    <div class="col-sm-4">
+      <h2>Sobre mim</h2>
+      <h5>Minha foto:</h5>
+      <div class="fakeimg"><img src="eu.png" width="250px" height="200px"></div>
+      <p>Aluno do cuso Técnico em Informática no Colégio Antonio Francisco Lisboa<br/>Jogador de APEX legends</p>
+      <h3>Alguns Links</h3>
+      <p>Redes Sociais</p>
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="https://www.instagram.com/gabriel.leonbs/">Instagram</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Facebook</a>
+        </li>
+      </ul>
+      <hr class="d-sm-none">
+    </div>
+    <div class="col-sm-8">
+      <h2>Apex Legends</h2>
+      <h5>Apex é um jogo recente lançado em fevereiro de 2019</h5>
+      <div class="fakeimg"><img src="apex.png" width="400px" height="200px"></div>
+      <p>Descrição..</p>
+      <p>É um jogo em battle royale jogado somente em equipes de 3 com um diferencial dos outros jogos em battle royale que seria
+      os seus personagens com diversas habilidades</p>
+      <br>
+      <h2>PERSONAGENS</h2>
+      <div class="fakeimg"><img src="personagens.jpg" width="400px" height="200px"></div>
+      <p>Por ordem da imagem</p>
+      <div class="row">
+      
+      
+      
+ <div class="card-deck mb-3 text-center">
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Free</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>10 users included</li>
+              <li>2 GB of storage</li>
+              <li>Email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Pro</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>20 users included</li>
+              <li>10 GB of storage</li>
+              <li>Priority email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
+          </div>
+        </div>      
+      </div>
+      
+           <!-- <<div class="col-sm-4">
+h3>Caustic</h3>
+      <div class="alert alert-primary" role="alert">
+  This is a primary alert—check it out!
+</div>
+      <p align= "justify">
+Dizem que nem todas as lendas são heróis, e isso é mais que evidente em Caustic. Muitos chamariam Caustic um sociopata. Imune a armas químicas, ele persegue os alvos com toda a calma. Ele usa armadilhas e granadas de gás tóxico para envolver em um instante todo um esquadrão.</p>
+   	</div>
+      <div class="col-sm-4">
+    <p>Bloodhound<br>
+Bloodhound é famoso nas Terras Extremas como um dos melhores caçadores que a Fronteira já viu – e isso é tudo o que se sabe sobre ele. As habilidades de rastreador de Bloodhound são inigualáveis e uma dádiva para qualquer equipe, pois ajudam a encontrar adversários ocultos e a rastrear a movimentação do inimigo. Tais capacidades táticas e de subjugação fazem de Bloodhound um adversário perigoso nos Apex Games.</p>
+     </div>
+     </div>-->
+    </div>
+  </div>
+</div>
+
+<?php
+ include("rodape.php");
+ ?>
